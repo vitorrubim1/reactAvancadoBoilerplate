ea2b95f6f0b4aea2a03f8f15811e1a6a56e47fb7
Vitor Rubim <vitorrubim1@hotmail.com>
2020-12-10 16:47:37 -0300

    Feat: adicionando o estilo global e configurando o absolute imports no tsconfig

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { AppProps } from "next/app";
+import Head from "next/head";
+
+import GlobalStyles from "styles/global";
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/images/icon-192.png" />
+        <link rel="apple-touch-icon" href="/images/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starter to work with Typescript, React, NextJS and Styled-components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default App;
